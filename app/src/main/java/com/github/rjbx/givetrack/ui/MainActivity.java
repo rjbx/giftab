64fca59877b2dcca9f3f588589b6f94c4eafd78f
rjbx <rjbx@users.noreply.github.com>
2019-01-14 14:05:18 -0500

    fix: Prevent multiple options launching on click of options item
    Break statement was added to definition for selection of MainActivity menu magnitude action
    
    See also: a0e2cdb, d44cde6

--- app/src/main/java/com/github/rjbx/givetrack/ui/MainActivity.java
+++ app/src/main/java/com/github/rjbx/givetrack/ui/MainActivity.java
@@ -146,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements
                 mClearDialog.show();
                 mClearDialog.getButton(android.app.AlertDialog.BUTTON_NEUTRAL).setTextColor(Color.GRAY);
                 mClearDialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.RED);
-                return false;
+                break;
             case R.id.action_magnitude:
                 View view = getLayoutInflater().inflate(R.layout.seekbar_main, new LinearLayout(this));
                 SeekBar seekbar = view.findViewById(R.id.main_seekbar);
@@ -162,6 +162,7 @@ public class MainActivity extends AppCompatActivity implements
                 mMagnitudeDialog.show();
                 mMagnitudeDialog.getButton(android.app.AlertDialog.BUTTON_NEUTRAL).setTextColor(Color.GRAY);
                 mMagnitudeDialog.getButton(android.app.AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.RED);
+                break;
             case R.id.action_date:
                 Calendar calendar = Calendar.getInstance();
                 DatePickerDialog datePicker = new DatePickerDialog(
