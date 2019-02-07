069fe625edca9cc068e40f3d4f956dcfc5b52311
rjbx <rjbx@users.noreply.github.com>
2019-02-07 10:27:18 -0500

    fix: Respond to clicks of disabled view
    RecordActity ListAdapter onBindViewHolder() was augmented with additional attribute toggling of focusable in touch mode to restore editable state on exiting dual pane mode
    
    See also: aea7dc8, f0f53e6

--- app/src/main/java/com/github/rjbx/givetrack/ui/RecordActivity.java
+++ app/src/main/java/com/github/rjbx/givetrack/ui/RecordActivity.java
@@ -352,6 +352,7 @@ public class RecordActivity extends AppCompatActivity implements
             holder.mIdView.setText(String.format("EIN: %s", ein));
             holder.mTimeView.setText(DateFormat.getDateInstance().format(new Date(time)));
             holder.mAmountView.setText(NumberFormat.getCurrencyInstance().format(impact));
+            holder.mAmountView.setFocusableInTouchMode(!isDualPane());
             holder.mAmountView.setFocusable(!isDualPane());
             holder.mAmountView.setClickable(isDualPane());
 
