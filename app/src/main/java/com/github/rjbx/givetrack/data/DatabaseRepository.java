22a4ad19e3ba14c4f1a5f68e9abbc28666c0bff4
rjbx <rjbx@users.noreply.github.com>
2019-02-16 14:50:03 -0500

    refactor: Genericize Uri retrieval
    See also: 6ad8453

--- app/src/main/java/com/github/rjbx/givetrack/data/DatabaseRepository.java
+++ app/src/main/java/com/github/rjbx/givetrack/data/DatabaseRepository.java
@@ -7,6 +7,8 @@ import android.database.DatabaseUtils;
 import android.net.Uri;
 
 import com.github.rjbx.givetrack.data.entry.Company;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.rjbx.givetrack.data.DatabaseContract.*;
@@ -16,38 +18,38 @@ import com.github.rjbx.givetrack.data.entry.Search;
 
 public final class DatabaseRepository {
 
-    static <T extends Company> void getEntries(Context context, List<T> entries) {
+    static <T extends Company> List<T> getCompaniesFromType(Context context, Class<T> entryType) {
+        List<T> entries = new ArrayList<>();
         Cursor cursor = context.getContentResolver().query(
-                Entry.CONTENT_URI_SEARCH, null, null, null, null
+                getCompanyUriFromType(entryType), null, null, null, null
         );
-        if (cursor != null) cursorToEntries(cursor, entries);
+        if (cursor != null) cursorToCompanies(cursor, entries);
+        return entries;
     }
 
-    static <T extends Company> void setEntries(Context context, List<T> companies) {
+    static <T extends Company> void setCompanies(Context context, List<T> companies) {
         ContentValues[] values = new ContentValues[companies.size()];
-        for (int i = 0; i < companies.size(); i++) {
-            values[i] = companies.get(i).toContentValues();
-        }
-        context.getContentResolver().bulkInsert(Entry.CONTENT_URI_SEARCH, values);
+        for (int i = 0; i < companies.size(); i++) values[i] = companies.get(i).toContentValues();
+        context.getContentResolver().bulkInsert(getCompanyUriFromType(companies.get(0).getClass()), values);
     }
 
-    public static <T extends Company> void cursorRowToEntry(Cursor cursor, T entry) {
+    static <T extends Company> void cursorRowToCompany(Cursor cursor, T entry) {
         ContentValues values = new ContentValues();
         DatabaseUtils.cursorRowToContentValues(cursor, values);
         entry.fromContentValues(values);
     }
 
-    public static <T extends Company> void cursorToEntries(Cursor cursor, List<T> entries) {
+    static <T extends Company> void cursorToCompanies(Cursor cursor, List<T> entries) {
         int i = 0;
         cursor.moveToFirst();
-        do cursorRowToEntry(cursor, entries.get(i++));
+        do cursorRowToCompany(cursor, entries.get(i++));
         while (cursor.moveToNext());
     }
 
-    public static <T extends Company> Uri getUriFromEntryType(Class<T> entryType) {
+    public static <T extends Company> Uri getCompanyUriFromType(Class<T> entryType) {
         if (entryType.isInstance(Search.class)) return Entry.CONTENT_URI_SEARCH;
         if (entryType.isInstance(Giving.class)) return Entry.CONTENT_URI_GIVING;
         if (entryType.isInstance(Record.class)) return Entry.CONTENT_URI_RECORD;
-        return null;
+        throw new IllegalArgumentException("T must implement Company interface");
     }
 }
