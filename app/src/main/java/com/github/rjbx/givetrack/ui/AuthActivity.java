ceaa79d32ce58aac63cf20dab1e866cefb548766
rjbx <rjbx@users.noreply.github.com>
2019-02-20 08:03:50 -0500

    fix: Initialize field from Loader before referencing within authentication sequence
    See also: 3a2d724

--- app/src/main/java/com/github/rjbx/givetrack/ui/AuthActivity.java
+++ app/src/main/java/com/github/rjbx/givetrack/ui/AuthActivity.java
@@ -69,78 +69,12 @@ public class AuthActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
         ButterKnife.bind(this);
-
-        getSupportLoaderManager().initLoader(DatabaseContract.LOADER_ID_USER, null, new DatabaseCallbacks(this));
-
         if (BuildConfig.DEBUG) Timber.plant(new Timber.DebugTree());
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
 
-        String launchingAction = getIntent().getAction();
-
-        // TODO: Enable user selection
-        final User user = mUsers.get(0);
-
-        if (launchingAction != null) {
-            switch (launchingAction) {
-                case ACTION_SIGN_OUT:
-                    mFirebaseDatabase.getReference("users").child(user.getUid())
-                            .updateChildren(user.toParameterMap())
-                            .addOnCompleteListener(updatedChildrenTask ->
-                                    AuthUI.getInstance().signOut(AuthActivity.this)
-                                            .addOnCompleteListener(signedOutTask -> {
-                                                finish();
-                                                startActivity(new Intent(AuthActivity.this, AuthActivity.class).setAction(Intent.ACTION_MAIN));
-                                                Toast.makeText(AuthActivity.this, getString(R.string.message_logout), Toast.LENGTH_SHORT).show();
-                                            }));
-                    break;
-                // TODO: Ensure all data is properly removed
-                case ACTION_DELETE_ACCOUNT:
-                    DatabaseService.startActionResetData(AuthActivity.this);
-                    FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
-                    if (firebaseUser == null || firebaseUser.getEmail() == null) return;
-                    String userId = firebaseUser.getUid();
-                    firebaseUser.reauthenticate(EmailAuthProvider.getCredential(firebaseUser.getEmail(), getString(R.string.message_password_request)))
-                            .addOnCompleteListener(signedOutTask ->
-                                    firebaseUser.delete().addOnCompleteListener(completedTask -> {
-                                        if (completedTask.isSuccessful()) {
-                                            mFirebaseDatabase.getReference("users").child(userId).removeValue()
-                                                    .addOnCompleteListener(removedValueTask -> {
-                                                        AuthUI.getInstance().signOut(this);
-                                                        finish();
-                                                        startActivity(new Intent(AuthActivity.this, AuthActivity.class).setAction(Intent.ACTION_MAIN));
-                                                        Toast.makeText(AuthActivity.this, getString(R.string.message_data_erase), Toast.LENGTH_LONG).show();
-                                                    });
-                                        }
-                                    })
-                            );
-                    break;
-                // TODO: Ensure new users are properly logged in
-                case Intent.ACTION_MAIN:
-                    if (mFirebaseAuth.getCurrentUser() == null) {
-
-                        List<AuthUI.IdpConfig> providers = new ArrayList<>();
-                        providers.add(new AuthUI.IdpConfig.GoogleBuilder().build());
-                        providers.add(new AuthUI.IdpConfig.EmailBuilder().build());
-                        providers.add(new AuthUI.IdpConfig.AnonymousBuilder().build());
-
-                        Intent signIn = AuthUI.getInstance().createSignInIntentBuilder()
-                                .setLogo(R.mipmap.ic_launcher_round)
-                                .setTosAndPrivacyPolicyUrls("https://github.com/rjbx/Givetrack", "https://github.com/rjbx/Givetrack")
-                                .setTheme(R.style.AppTheme_AuthOverlay)
-                                .setIsSmartLockEnabled(false, true)
-                                .setAvailableProviders(providers)
-                                .build();
-                        startActivityForResult(signIn, REQUEST_SIGN_IN);
-
-                    } else {
-                        finish();
-                        Toast.makeText(this, getString(R.string.message_login), Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(this, MainActivity.class).setAction(AuthActivity.ACTION_SIGN_IN));
-                    }
-            }
-        }
+        getSupportLoaderManager().initLoader(DatabaseContract.LOADER_ID_USER, null, new DatabaseCallbacks(this));
     }
 
     /**
@@ -212,10 +146,79 @@ public class AuthActivity extends AppCompatActivity implements
     public void onLoadFinished(int id, Cursor cursor) {
         DatabaseAccessor.cursorToEntries(cursor, mUsers);
         if (mUsers == null || mUsers.isEmpty()) mUsers.add(new User());
+        handleAction();
     }
 
     @Override
     public void onLoaderReset() {
         mUsers = null;
+    }
+
+    private void handleAction() {
+
+        String launchingAction = getIntent().getAction();
+
+        // TODO: Enable user selection
+        final User user = mUsers.get(0);
+
+        if (launchingAction != null) {
+            switch (launchingAction) {
+                case ACTION_SIGN_OUT:
+                    mFirebaseDatabase.getReference("users").child(user.getUid())
+                            .updateChildren(user.toParameterMap())
+                            .addOnCompleteListener(updatedChildrenTask ->
+                                    AuthUI.getInstance().signOut(AuthActivity.this)
+                                            .addOnCompleteListener(signedOutTask -> {
+                                                finish();
+                                                startActivity(new Intent(AuthActivity.this, AuthActivity.class).setAction(Intent.ACTION_MAIN));
+                                                Toast.makeText(AuthActivity.this, getString(R.string.message_logout), Toast.LENGTH_SHORT).show();
+                                            }));
+                    break;
+                // TODO: Ensure all data is properly removed
+                case ACTION_DELETE_ACCOUNT:
+                    DatabaseService.startActionResetData(AuthActivity.this);
+                    FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                    if (firebaseUser == null || firebaseUser.getEmail() == null) return;
+                    String userId = firebaseUser.getUid();
+                    firebaseUser.reauthenticate(EmailAuthProvider.getCredential(firebaseUser.getEmail(), getString(R.string.message_password_request)))
+                            .addOnCompleteListener(signedOutTask ->
+                                    firebaseUser.delete().addOnCompleteListener(completedTask -> {
+                                        if (completedTask.isSuccessful()) {
+                                            mFirebaseDatabase.getReference("users").child(userId).removeValue()
+                                                    .addOnCompleteListener(removedValueTask -> {
+                                                        AuthUI.getInstance().signOut(this);
+                                                        finish();
+                                                        startActivity(new Intent(AuthActivity.this, AuthActivity.class).setAction(Intent.ACTION_MAIN));
+                                                        Toast.makeText(AuthActivity.this, getString(R.string.message_data_erase), Toast.LENGTH_LONG).show();
+                                                    });
+                                        }
+                                    })
+                            );
+                    break;
+                // TODO: Ensure new users are properly logged in
+                case Intent.ACTION_MAIN:
+                    if (mFirebaseAuth.getCurrentUser() == null) {
+
+                        List<AuthUI.IdpConfig> providers = new ArrayList<>();
+                        providers.add(new AuthUI.IdpConfig.GoogleBuilder().build());
+                        providers.add(new AuthUI.IdpConfig.EmailBuilder().build());
+                        providers.add(new AuthUI.IdpConfig.AnonymousBuilder().build());
+
+                        Intent signIn = AuthUI.getInstance().createSignInIntentBuilder()
+                                .setLogo(R.mipmap.ic_launcher_round)
+                                .setTosAndPrivacyPolicyUrls("https://github.com/rjbx/Givetrack", "https://github.com/rjbx/Givetrack")
+                                .setTheme(R.style.AppTheme_AuthOverlay)
+                                .setIsSmartLockEnabled(false, true)
+                                .setAvailableProviders(providers)
+                                .build();
+                        startActivityForResult(signIn, REQUEST_SIGN_IN);
+
+                    } else {
+                        finish();
+                        Toast.makeText(this, getString(R.string.message_login), Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(this, MainActivity.class).setAction(AuthActivity.ACTION_SIGN_IN));
+                    }
+            }
+        }
     }
 }
