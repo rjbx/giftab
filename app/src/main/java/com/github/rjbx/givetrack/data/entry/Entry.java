639b7c2d5e691286c060c015c9c824f878607589
rjbx <rjbx@users.noreply.github.com>
2019-02-22 08:49:51 -0500

    fix: Remove unused method
    See also: a4e62f6, 5d72926

--- app/src/main/java/com/github/rjbx/givetrack/data/entry/Entry.java
+++ app/src/main/java/com/github/rjbx/givetrack/data/entry/Entry.java
@@ -2,18 +2,8 @@ package com.github.rjbx.givetrack.data.entry;
 
 import android.content.ContentValues;
 
-import com.google.firebase.database.Exclude;
-
-import timber.log.Timber;
-
 public interface Entry {
 
     ContentValues toContentValues();
     void fromContentValues(ContentValues values);
-    @Exclude static <T extends Entry> T getInstance(Class<T> type) {
-        T entry = null;
-        try { entry = type.newInstance(); }
-        catch (InstantiationException|IllegalAccessException e) { Timber.e(e); }
-        return entry;
-    }
 }
