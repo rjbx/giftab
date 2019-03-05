523f0dea84991734c8f257096d69b1f4235d6344
rjbx <rjbx@users.noreply.github.com>
2019-03-05 17:25:13 -0500

    fix: Prevent overwrite of User when updating attribute

--- app/src/main/java/com/github/rjbx/givetrack/data/DatabaseAccessor.java
+++ app/src/main/java/com/github/rjbx/givetrack/data/DatabaseAccessor.java
@@ -279,8 +279,11 @@ public final class DatabaseAccessor {
      */
     static <T extends Entry> /*Task<Void>*/void addEntriesToRemote(FirebaseDatabase remote, Class<T> entryType, T... entries) {
 
+        Map<String, Object> map = new HashMap<>();
+        map.put("giveStamp", System.currentTimeMillis());
+
         DatabaseReference userReference = remote.getReference(User.class.getSimpleName().toLowerCase());
-        userReference.child(entries[0].getUid()).child(DatabaseContract.getTimeTableColumn(entryType)).setValue(System.currentTimeMillis());
+        userReference.child(entries[0].getUid()).updateChildren(map);
 
         String entryPath = entryType.getSimpleName().toLowerCase();
         DatabaseReference entryReference = remote.getReference(entryPath);
@@ -320,8 +323,11 @@ public final class DatabaseAccessor {
 
     static <T extends Entry> void removeEntriesFromRemote(FirebaseDatabase remote, Class<T> entryType, @Nullable T... entries) {
 
+        Map<String, Object> map = new HashMap<>();
+        map.put("giveStamp", System.currentTimeMillis());
+
         DatabaseReference userReference = remote.getReference(User.class.getSimpleName().toLowerCase());
-        userReference.child(entries[0].getUid()).child(DatabaseContract.getTimeTableColumn(entryType)).setValue(System.currentTimeMillis());
+        userReference.child(entries[0].getUid()).updateChildren(map);
 
         DatabaseReference reference = remote.getReference(entryType.getSimpleName().toLowerCase());
 
