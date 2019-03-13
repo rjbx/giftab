3e95fd8c6ce6544852fab1fe0d5635ac59a0aebe
rjbx <rjbx@users.noreply.github.com>
2019-03-13 00:55:17 -0400

    refactor: Update argument to resolve to overload method signature

--- app/src/main/java/com/github/rjbx/givetrack/ui/GiveFragment.java
+++ app/src/main/java/com/github/rjbx/givetrack/ui/GiveFragment.java
@@ -648,7 +648,7 @@ public class GiveFragment extends Fragment implements
                             Target target = (Target) mRemoveDialog.getButton(AlertDialog.BUTTON_NEGATIVE).getTag();
                             if (sDualPane) showSinglePane();
 //                                if (sValuesArray.length == 1) onDestroy();
-                            DatabaseService.startActionRemoveTarget(getContext(), target);
+                            DatabaseService.startActionRemoveTarget(getContext(), target.getEin());
                             break;
                         default:
                     }
