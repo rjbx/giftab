c51b12077dfa3175084c299322340d8f381ef552
rjbx <rjbx@users.noreply.github.com>
2019-04-07 15:07:42 -0400

    chore: Add annotation

--- app/src/main/java/com/github/rjbx/givetrack/view/GiveFragment.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/GiveFragment.java
@@ -58,6 +58,7 @@ import java.util.TimerTask;
 import static com.github.rjbx.givetrack.AppUtilities.CURRENCY_FORMATTER;
 import static com.github.rjbx.givetrack.AppUtilities.PERCENT_FORMATTER;
 
+// TODO: Prevent errant removal of target adjacent to selected removal
 /**
  * Provides the logic and views for a user activity management screen.
  */
