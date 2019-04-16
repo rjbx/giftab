1ca449c3c02c773a6a97af8d7df128fd6e873bee
rjbx <rjbx@users.noreply.github.com>
2019-04-16 19:22:40 -0400

    chore: Add annotation

--- app/src/main/java/com/github/rjbx/givetrack/view/AuthActivity.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/AuthActivity.java
@@ -47,6 +47,7 @@ import java.util.List;
 import static android.content.Intent.ACTION_MAIN;
 import static com.github.rjbx.givetrack.data.DatabaseContract.LOADER_ID_USER;
 
+// TODO: Repair OAuth signin
 /**
  * Provides a UI for and manages user authentication interfacing with {@link FirebaseAuth}.
  */
