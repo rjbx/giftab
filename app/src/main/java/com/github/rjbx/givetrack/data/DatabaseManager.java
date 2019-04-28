ab0b348b7934c3ecde47b5ea5ecaa8f9c7a283d8
rjbx <rjbx@users.noreply.github.com>
2019-04-28 11:13:05 -0400

    refactor: Amend list comparison logic

--- app/src/main/java/com/github/rjbx/givetrack/data/DatabaseManager.java
+++ app/src/main/java/com/github/rjbx/givetrack/data/DatabaseManager.java
@@ -698,7 +698,7 @@ public final class DatabaseManager extends IntentService {
         List<Target> targetList = DatabaseAccessor.getTarget(this);
         if (targetList.size() - 1 == targets.length) {
             for (int i = 0; i < targetList.size(); i++) {
-                if (i > targets.length) break;
+                if (i - offset >= targets.length) break;
                 if (targetList.get(i).getId().equals(targets[i - offset].getId())) matchCount++;
                 else offset++;
             }
