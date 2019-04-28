ed735f9f8d71acb0ebe8f1e493ce19b9ffc3d328
rjbx <rjbx@users.noreply.github.com>
2019-04-28 08:48:53 -0400

    chore: Update annotation

--- app/src/main/java/com/github/rjbx/givetrack/data/DatabaseAccessor.java
+++ app/src/main/java/com/github/rjbx/givetrack/data/DatabaseAccessor.java
@@ -176,6 +176,7 @@ public final class DatabaseAccessor {
         FirebaseDatabase remote = FirebaseDatabase.getInstance();
 
         long stamp = System.currentTimeMillis();
+        // TODO: Either replace full list on single element target editions or set reset to false, discern removal and invoke removeTarget from GiveFragment sync
         addEntriesToLocal(local, Target.class, stamp, true, entries);
         addEntriesToRemote(remote, Target.class, stamp, true, entries);
     }
