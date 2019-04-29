1d58b3a2ba2a78f17b82b7606161fffe54df5e8e
rjbx <rjbx@users.noreply.github.com>
2019-04-29 09:04:40 -0400

    chore: Update annotation

--- app/src/main/java/com/github/rjbx/givetrack/view/GiveFragment.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/GiveFragment.java
@@ -44,7 +44,6 @@ import timber.log.Timber;
 import com.github.rjbx.calibrater.Calibrater;
 import com.github.rjbx.givetrack.AppUtilities;
 import com.github.rjbx.givetrack.R;
-import com.github.rjbx.givetrack.data.DatabaseAccessor;
 import com.github.rjbx.givetrack.data.DatabaseManager;
 import com.github.rjbx.givetrack.data.entry.Target;
 import com.github.rjbx.givetrack.data.entry.User;
@@ -62,6 +61,7 @@ import static com.github.rjbx.givetrack.AppUtilities.CURRENCY_FORMATTER;
 import static com.github.rjbx.givetrack.AppUtilities.PERCENT_FORMATTER;
 
 
+// TODO: Replace remove button with type toggle and restore target update from detail
 /**
  * Provides the logic and views for a user activity management screen.
  */
@@ -445,21 +445,6 @@ public class GiveFragment extends Fragment implements
         ListAdapter(List<Target> targetList) {
             mTargetList = targetList;
             mObjects = Rateraid.with(mTargetList, mMagnitude, Calibrater.STANDARD_PRECISION, clickedView -> {
-                //                float sum = 0;
-//                for (double percentage : sPercentages) sum += percentage;
-//                Timber.d("List[%s] : Sum[%s]", Arrays.asList(sPercentages).toString(), sum);
-
-//                if (mContext == null) return;
-//                AlertDialog dialog = new AlertDialog.Builder(mContext).create();
-//                dialog.setMessage(mContext.getString(R.string.message_remove_entry, mTargetList.get((int) clickedView.getTag()), "collection"));
-//                dialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.dialog_option_keep), new Message());
-//                dialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.dialog_option_remove), new Message());
-//                dialog.show();
-//                dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(getResources().getColor(R.color.colorNeutralDark, null));
-//                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(getResources().getColor(R.color.colorAttentionDark, null));
-//                dialog.getButton(AlertDialog.BUTTON_NEUTRAL).setOnClickListener(clickedNeutral -> dialog.dismiss());
-//                dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(clickedNegative -> {});
-//
                 sPercentagesAdjusted = true;
                 scheduleSyncPercentages();
                 renderActionBar();
