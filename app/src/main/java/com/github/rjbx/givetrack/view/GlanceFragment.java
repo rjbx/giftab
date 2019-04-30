708996e59faf87bec94c2d770f18a822f14fe162
rjbx <rjbx@users.noreply.github.com>
2019-04-30 07:13:41 -0400

    chore: Update annotation

--- app/src/main/java/com/github/rjbx/givetrack/view/GlanceFragment.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/GlanceFragment.java
@@ -71,6 +71,7 @@ import static com.github.rjbx.givetrack.AppUtilities.DATE_FORMATTER;
 //   1: Click amount and transfer color change to header
 //   2: Long click tracked label
 //   3: Options menu
+//   4: Button placement to left of amount (with type indicator to right of amount)
 //  B: Replace share with type button and access share button from header lower right corner
 /**
  * Provides the logic and views for a user activity overview screen.
