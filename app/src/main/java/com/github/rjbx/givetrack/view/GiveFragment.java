0ee1f20aefc35f3407939462fe78477818f5c53d
rjbx <rjbx@users.noreply.github.com>
2019-05-15 07:09:45 -0400

    chore: Update annotation

--- app/src/main/java/com/github/rjbx/givetrack/view/GiveFragment.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/GiveFragment.java
@@ -62,6 +62,7 @@ import java.util.TimerTask;
 import static com.github.rjbx.givetrack.AppUtilities.CURRENCY_FORMATTER;
 import static com.github.rjbx.givetrack.AppUtilities.PERCENT_FORMATTER;
 
+// TODO: Ensure automatic reset and removal restoration do not occur on target list changes
 /**
  * Provides the logic and views for a user activity management screen.
  */
