34291fd586737c5002633a6392ded7892896b3e3
rjbx <rjbx@users.noreply.github.com>
2019-05-17 19:38:52 -0400

    refactor: Remove refresh call leading to recursion through onUpdate

--- app/src/main/java/com/github/rjbx/givetrack/AppWidget.java
+++ app/src/main/java/com/github/rjbx/givetrack/AppWidget.java
@@ -56,7 +56,6 @@ public class AppWidget extends AppWidgetProvider {
         views.setOnClickPendingIntent(R.id.widget_record, recordPendingIntent);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
-        refresh(context);
     }
 
     /**
