bf5838ab27984cab8894a20d374f6fffc6b006ba
rjbx <rjbx@users.noreply.github.com>
2019-05-30 13:12:03 -0400

    chore: Update annotation

--- app/src/main/java/com/github/rjbx/givetrack/view/RewardActivity.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/RewardActivity.java
@@ -45,19 +45,14 @@ import static com.github.rjbx.givetrack.data.DatabaseContract.LOADER_ID_USER;
 //  B. Processing fee waiver:
 //      1. Start fee waiver count at 0 daily by default
 //      2. Display checkout dialog or activity on clicking conversion button with payments preference set to true
-//      3. Access rewarded locally from button click displayed beside fee checkout line item
-//      4. Increment fee waiver count for each credit
-//      5. Display fee waiver count locally from text beside fee checkout line item
-//      6. Process transaction with batch payment processing service
-//          a. Charity Navigator website
-//              i. Start WebViewClient session
-//              ii. Open links to add to cart followed by cart
-//              iii. Listen for submission link open
-//              iv. If no credits, open dialog providing option to waive transaction fee
+//      3a. Access rewarded locally from button click displayed beside fee checkout line item
+//      3b. If no credits, open dialog providing option to waive transaction fee
 //              v. Indicate number of transactions as well as fee per transaction ($0.10)
 //              vi. Indicate that after ten transactions $0.99 will be billed
 //              vii. Each credit covers ten transaction fee waivers
-
+//      4. Increment fee waiver count for each credit
+//      5. Display fee waiver count locally from text beside fee checkout line item
+//      6. Process transaction with batch payment processing service
 /**
  * Provides a RewardedAd by which to augment balance toward donations.
  */
