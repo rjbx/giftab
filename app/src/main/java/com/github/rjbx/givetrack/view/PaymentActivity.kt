12fd9039a739ff973dbd9c83d8d22d61371f6fef
rjbx <rjbx@users.noreply.github.com>
2019-07-21 11:29:16 -0400

    feature: Add class and resource for payment logic and views

---
+++ app/src/main/java/com/github/rjbx/givetrack/view/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.github.rjbx.givetrack.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.id.activity_payment);
+    }
+}
