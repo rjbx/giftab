009793c586d373d53d132a16b21314c0d66f628d
rjbx <rjbx@users.noreply.github.com>
2019-07-21 20:33:31 -0400

    refactor: Set up LoaderCallbacks for payment data

--- app/src/main/java/com/github/rjbx/givetrack/view/RemitActivity.java
+++ app/src/main/java/com/github/rjbx/givetrack/view/RemitActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 import android.widget.Button;
+import android.database.Cursor;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import com.github.rjbx.givetrack.AppUtilities;
 import com.github.rjbx.givetrack.R;
+import com.github.rjbx.givetrack.data.DatabaseContract;
+import com.github.rjbx.givetrack.data.entry.Record;
+import com.github.rjbx.givetrack.data.entry.User;
 import com.google.android.gms.wallet.*;
 import com.google.android.gms.common.api.*;
+import com.google.android.material.snackbar.Snackbar;
 import com.stripe.android.model.Token;
 
 import java.util.Arrays;
@@ -21,14 +31,20 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import timber.log.Timber;
 
+import static com.github.rjbx.givetrack.data.DatabaseContract.LOADER_ID_RECORD;
+import static com.github.rjbx.givetrack.data.DatabaseContract.LOADER_ID_TARGET;
+import static com.github.rjbx.givetrack.data.DatabaseContract.LOADER_ID_USER;
+
 
 /**
  * Provides a UI for and manages payment confirmation and processing.
  */
-public class RemitActivity extends AppCompatActivity {
+public class RemitActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final int LOAD_PAYMENT_DATA_REQUEST_CODE = 1;
     private float mPrice;
+    private boolean mLock;
+    private User mUser;
     private PaymentsClient mPaymentsClient;
     @BindView(R.id.remit_action_bar) Button mConfirmButton;
 
@@ -38,24 +54,18 @@ public class RemitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_remit);
         ButterKnife.bind(this);
 
-        mPaymentsClient
-                = Wallet.getPaymentsClient(this, new Wallet
-                        .WalletOptions.Builder()
-                        .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
-                        .build()
-        );
+        getSupportLoaderManager().initLoader(LOADER_ID_USER, null, this);
 
-        isReadyToPay();
-    }
+        if (mUser != null) {
+            mPaymentsClient
+                    = Wallet.getPaymentsClient(this, new Wallet
+                    .WalletOptions.Builder()
+                    .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                    .build()
+            );
 
-    @OnClick(R.id.remit_action_bar) void confirmPayment() {
-            PaymentDataRequest request = createPaymentDataRequest();
-            if (request != null) {
-                AutoResolveHelper.resolveTask(
-                        mPaymentsClient.loadPaymentData(request),
-                        this,
-                        LOAD_PAYMENT_DATA_REQUEST_CODE);
-            }
+            isReadyToPay();
+        }
     }
 
     @Override
@@ -99,6 +109,65 @@ public class RemitActivity extends AppCompatActivity {
             Timber.e("Payment data is unavailable");
         }
     }
+
+    /**
+     * Defines the data to be returned from {@link LoaderManager.LoaderCallbacks}.
+     */
+    @NonNull
+    @Override public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        switch (id) {
+            case LOADER_ID_TARGET: return new CursorLoader(this, DatabaseContract.CompanyEntry.CONTENT_URI_TARGET, null, DatabaseContract.CompanyEntry.COLUMN_UID + " = ? ", new String[] { mUser.getUid() }, null);
+            case LOADER_ID_RECORD: return new CursorLoader(this, DatabaseContract.CompanyEntry.CONTENT_URI_RECORD, null, DatabaseContract.CompanyEntry.COLUMN_UID + " = ? ", new String[] { mUser.getUid() }, mUser.getJournalSort() + " " + mUser.getJournalOrder());
+            case LOADER_ID_USER: return new CursorLoader(this, DatabaseContract.UserEntry.CONTENT_URI_USER, null, DatabaseContract.UserEntry.COLUMN_USER_ACTIVE + " = ? ", new String[] { "1" }, null);
+            default: throw new RuntimeException(this.getString(R.string.loader_error_message, id));
+        }
+    }
+
+    /**
+     * Replaces old data that is to be subsequently released from the {@link Loader}.
+     */
+    @Override public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        if (data == null || (!data.moveToFirst())) return;
+        int id = loader.getId();
+        switch (id) {
+            case DatabaseContract.LOADER_ID_TARGET:
+                break;
+            case DatabaseContract.LOADER_ID_RECORD:
+                if (mLock) break;
+                break;
+            case DatabaseContract.LOADER_ID_USER:
+                if (data.moveToFirst()) {
+                    do {
+                        User user = User.getDefault();
+                        AppUtilities.cursorRowToEntry(data, user);
+                        if (user.getUserActive()) {
+                            mLock = false;
+                            mUser = user;
+                        }
+                    } while (data.moveToNext());
+                }
+                break;
+            default:
+                throw new RuntimeException(getString(R.string.loader_error_message, id));
+        }
+    }
+
+    /**
+     * Tells the application to remove any stored references to the {@link Loader} data.
+     */
+    @Override public void onLoaderReset(@NonNull Loader<Cursor> loader) { }
+
+
+    @OnClick(R.id.remit_action_bar) void confirmPayment() {
+        PaymentDataRequest request = createPaymentDataRequest();
+        if (request != null) {
+            AutoResolveHelper.resolveTask(
+                    mPaymentsClient.loadPaymentData(request),
+                    this,
+                    LOAD_PAYMENT_DATA_REQUEST_CODE);
+        }
+    }
+
 
     private void chargeToken(String tokenId) {
 
